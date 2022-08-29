54a555dfeb9edddf3e06d3f0b0ea0b6150980c94
Bongiwe Tshabalala <u13058879@tuks.co.za>
2022-08-29 19:59:28 +0200

    First Commit

---
+++ Task 1/Data/Social Buzz Script.py
@@ -0,0 +1,7 @@
+# cd 'C:/Users/bongi/OneDrive/Bongiwe_Documents/Academics/Forage/'
+# git config --global user.name "Bongiwe Tshabalala"
+# git config --global user.email u13058879@tuks.co.za
+# git config --global init.default branch main
+# git help config
+# git init
+# git status

4dafb4ceecbcc0c5f9db7409c8d5f30fab6431c1
Bongiwe Tshabalala <u13058879@tuks.co.za>
2022-08-31 12:12:32 +0200

    first data file added

--- Task 1/Data/Social Buzz Script.py
+++ Task 1/Data/Social Buzz Script.py
@@ -1,7 +1,3 @@
-# cd 'C:/Users/bongi/OneDrive/Bongiwe_Documents/Academics/Forage/'
-# git config --global user.name "Bongiwe Tshabalala"
-# git config --global user.email u13058879@tuks.co.za
-# git config --global init.default branch main
-# git help config
-# git init
-# git status
+import pandas as pd
+SB_Content = pd.read_csv(r"C:\Users\bongi\OneDrive\Bongiwe_Documents\Academics\Forage\Task 1\Data\Content (1).csv")
+print(SB_Content)

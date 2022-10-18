da41ccfb93c6342bcad5b311023985b29033f2ef
Bongiwe Tshabalala <u13058879@tuks.co.za>
2022-10-18 10:30:18 +0200

    Rearranged folders

--- Task 1/Data/Git_Pull.bat
+++
@@ -1,8 +0,0 @@
-cd 'C:/Users/bongi/OneDrive/Bongiwe_Documents/Academics/Forage/'
-git config --global user.name "Bongiwe Tshabalala"
-git config --global user.email u13058879@tuks.co.za
-git config --global init.default branch main
-git add --all
-git commit -m "Exported .csv Files"
-git branch -M main
-git push -u origin main
